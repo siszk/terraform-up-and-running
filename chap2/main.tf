ea5268df2e375b69ff5af4016b82e91b22df1d53
shuhei.ishizuka <shusan.17@gmail.com>
2024-06-01 17:53:08 +0900

    mod: replace instance launch setting

--- chap2/main.tf
+++ chap2/main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-01bef798938b7644d"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-01bef798938b7644d"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
   #!/bin/bash
@@ -13,10 +13,22 @@ resource "aws_instance" "example" {
   nohup busybox httpd -f -p ${var.server_port} &
   EOF
 
-  user_data_replace_on_change = true
+  # lifecycle {
+  #   create_before_destroy = true
+  # }
+}
 
-  tags = {
-    Name = "example-terraform"
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
 
@@ -37,7 +49,13 @@ variable "server_port" {
   default     = 8080
 }
 
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP address of the web server"
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
