b1d3764d6e2056be3b539bf8ee8441a195c8abde
shuhei.ishizuka <shusan.17@gmail.com>
2024-06-08 15:01:05 +0900

    mod: use instance var

--- chap4/modules/services/webserver-cluster/main.tf
+++ chap4/modules/services/webserver-cluster/main.tf
@@ -10,7 +10,7 @@ terraform {
 
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-01bef798938b7644d"
-  instance_type   = "t2.micro"
+  instance_type   = var.instance_type
   security_groups = [aws_security_group.instance.id]
 
   user_data = templatefile("user-data.sh", {
@@ -31,8 +31,8 @@ resource "aws_autoscaling_group" "example" {
   target_group_arns = [aws_lb_target_group.asg.arn]
   health_check_type = "ELB"
 
-  min_size = 2
-  max_size = 10
+  min_size = var.min_size
+  max_size = var.max_size
 
   tag {
     key                 = "Name"
