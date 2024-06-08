7334cae3f610cc11a6e54586464601d63d90d1bb
shuhei.ishizuka <shusan.17@gmail.com>
2024-06-08 15:02:37 +0900

    mod: set instance vars

--- chap4/stage/services/webserver-cluster/main.tf
+++ chap4/stage/services/webserver-cluster/main.tf
@@ -8,4 +8,8 @@ module "webserver-cluster" {
   cluster_name           = "webservers-stage"
   db_remote_state_bucket = "terraform-up-and-running-2024060202-state"
   db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
+
+  instance_type = "t2.micro"
+  min_size      = 2
+  max_size      = 2
 }
